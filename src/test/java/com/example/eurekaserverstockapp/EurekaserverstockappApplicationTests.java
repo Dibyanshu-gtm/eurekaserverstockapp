48529de1cf0b182663222eb6dc25831b31cf5c5c
Dibyanshu Gautam <dibyanshu.gtm@gmail.com>
2021-07-29 13:36:26 +0530

    Initial Commit

---
+++ src/test/java/com/example/eurekaserverstockapp/EurekaserverstockappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekaserverstockapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverstockappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
